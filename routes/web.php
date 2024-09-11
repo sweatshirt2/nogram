e84c81711773b25266065b0e08ee3332367d5f5f
sweatshirt <sweatshirtblue2@gmail.com>
2024-09-11 09:48:33 +0300

    models migrations relationships factories

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/posts', function () {
+    $posts = Post::getAll();
+    return view('posts', compact('posts'));
+});
+
+Route::get('/posts/{id}', function ($id) {
+    $post = Post::getById(intval($id));
+    return view('post', compact('post'));
+});
+
+Route::get('/people', function () {
+    return view('people');
 });
